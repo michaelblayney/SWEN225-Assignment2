88426df862d153a030ea8c627cd9a2e1ae0068b1
jessie-bp <jessie.bukholtpayne@gmail.com>
2020-08-20 14:56:53 +1200

    added mouselistener stuff

--- SWEN225-Assignment2/src/code/BoardPanel.java
+++ SWEN225-Assignment2/src/code/BoardPanel.java
@@ -1,10 +1,43 @@
 package code;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import javax.swing.JPanel;
 
 /* Custom JPanel class to draw board and possibly receive mouse input.
  * 
  */
-public class BoardPanel extends JPanel {
+public class BoardPanel extends JPanel implements MouseListener{
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
