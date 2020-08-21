5716b18ab35bb45d4c04f871fc86a0c2c345de9f
RFeite <68829916+RFeite@users.noreply.github.com>
2020-08-21 23:26:28 +1200

    --

--- SWEN225-Assignment2/src/code/CardsPanel.java
+++ SWEN225-Assignment2/src/code/CardsPanel.java
@@ -1,10 +1,15 @@
 package code;
 
-import javax.swing.JPanel;
+import javax.swing.*;
 
 /* Custom JPanel class to display cards in current player's hand
  *  and possibly receive mouse input.
  */
 public class CardsPanel extends JPanel {
-
+	
+	public CardsPanel() {
+		JLabel label = new JLabel("Cards");
+		add(label);
+	}
+	
 }
